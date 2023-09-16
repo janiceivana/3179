c28c0ce0a643ff7c7e6f4bb05269392ac41f4848
Janice Ivana <jiva0001@student.monash.edu>
2023-09-16 21:09:26 +1000

    upload all folder and files needed

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,49 @@
+var spec = "js/line_chart.json";
+vegaEmbed('#line', spec).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec2 = "js/bar_chart.json";
+vegaEmbed('#bar_chart', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec3 = "js/map.json";
+vegaEmbed('#map', spec3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec4 = "js/stacked_area_chart.json";
+vegaEmbed('#area', spec4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec5 = "js/boxplot.json";
+vegaEmbed('#boxplot', spec5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec6 = "js/donut_chart.json";
+vegaEmbed('#donut', spec6).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec7 = "js/histogram.json";
+vegaEmbed('#hist', spec7).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec8 = "js/barbell_chart.json";
+vegaEmbed('#barbell', spec8).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec9 = "js/bubble_chart.json";
+vegaEmbed('#bubble', spec9).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec10 = "js/grouped_bar_chart.json";
+vegaEmbed('#grouped_bar', spec10).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
