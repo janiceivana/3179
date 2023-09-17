0d8c5acb6d58037dcd5af3552efab15b04e5a9c1
Janice Ivana <jiva0001@student.monash.edu>
2023-09-17 21:54:06 +1000

    add new chart

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -47,3 +47,105 @@ var spec10 = "js/grouped_bar_chart.json";
 vegaEmbed('#grouped_bar', spec10).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+
+// var spec = "js/line_chart.json";
+// vegaEmbed('#line', spec).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec2 = "js/bar_chart.json";
+// vegaEmbed('#bar_chart', spec2).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec3 = "js/map.json";
+// vegaEmbed('#map', spec3).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec4 = "js/stacked_area_chart.json";
+// vegaEmbed('#area', spec4).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec5 = "js/boxplot.json";
+// vegaEmbed('#boxplot', spec5).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec6 = "js/donut_chart.json";
+// vegaEmbed('#donut', spec6).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec7 = "js/histogram.json";
+// vegaEmbed('#hist', spec7).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec8 = "js/barbell_chart.json";
+// vegaEmbed('#barbell', spec8).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+var spec19 = "js/bubble_chart_pediatric.json";
+vegaEmbed('#bubble_pediatric', spec19).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+// var spec10 = "js/grouped_bar_chart.json";
+// vegaEmbed('#grouped_bar', spec10).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+
+// var spec = "js/line_chart.json";
+// vegaEmbed('#line', spec).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec2 = "js/bar_chart.json";
+// vegaEmbed('#bar_chart', spec2).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec3 = "js/map.json";
+// vegaEmbed('#map', spec3).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec4 = "js/stacked_area_chart.json";
+// vegaEmbed('#area', spec4).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec5 = "js/boxplot.json";
+// vegaEmbed('#boxplot', spec5).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec6 = "js/donut_chart.json";
+// vegaEmbed('#donut', spec6).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec7 = "js/histogram.json";
+// vegaEmbed('#hist', spec7).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+// var spec8 = "js/barbell_chart.json";
+// vegaEmbed('#barbell', spec8).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+var spec29 = "js/bubble_chart_pregnant.json";
+vegaEmbed('#bubble_pregnant', spec29).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+// var spec10 = "js/grouped_bar_chart.json";
+// vegaEmbed('#grouped_bar', spec10).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
